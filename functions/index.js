33670220b296211e3c8267d97629e4d8d07902ce
md.haider <md.haider@cliksource.com>
2019-10-16 23:08:48 +0530

    minor change

--- functions/index.js
+++ functions/index.js
@@ -1,20 +1,42 @@
-const functions = require('firebase-functions');
+'use strict';
+
 const express = require('express');
 const cors = require('cors');
-const admin = require('firebase-admin');
 
+// Firebase init
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+// Express and CORS middleware init
 const app = express();
 app.use(cors());
 
-admin.initializeApp();
+// POST / method
+app.post("/", (request, response) => {
+    const entry = request.body;
 
+    return admin.database().ref('/entries').push(entry)
+        .then(() => {
+            return response.status(200).send(entry)
+        }).catch(error => {
+            console.error(error);
+            return response.status(500).send('Oh no! Error: ' + error);
+        });
+});
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
+// GET / method
+app.get("/", (request, response) => {
+    return admin.database().ref('/entries').on("value", snapshot => {
+        return response.status(200).send(snapshot.val());
+    }, error => {
+        console.error(error);
+        return response.status(500).send('Oh no! Error: ' + error);
+    });
+});
 
- exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
-  });
-  
-  exports.entries= functions.https.onReques(app);
-  
+exports.entries = functions.https.onRequest(app);
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+    response.send("Hello from Firebase!");
+});
